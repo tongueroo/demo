8453487acdfd4a2b1166ccbcb6b99ef0facc4618
Tung Nguyen <tongueroo@gmail.com>
2017-11-02 11:06:26 -0700

    log event

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    Jets.logger.info("event #{event.inspect}")
     posts = Post.scan # should not use scan for production
     posts.each { |item| Jets.logger.info(item.inspect) }
     render json: {posts: posts.items}, status: 200
@@ -10,22 +11,27 @@ class PostsController < ApplicationController
   end
 
   def show
+    Jets.logger.info("event #{event.inspect}")
     render json: {a: "show"}, status: 200
   end
 
   def create
+    Jets.logger.info("event #{event.inspect}")
     render json: {a: "create"}, status: 200
   end
 
   def edit
+    Jets.logger.info("event #{event.inspect}")
     render json: {a: "edit"}, status: 200
   end
 
   def update
+    Jets.logger.info("event #{event.inspect}")
     render json: {a: "update"}, status: 200
   end
 
   def delete
+    Jets.logger.info("event #{event.inspect}")
     render json: {a: "delete"}, status: 200
   end
 end
