9bb392d1da44aa4bd7729c8cc3ce7fab16882728
Tung Nguyen <tongueroo@gmail.com>
2017-11-14 10:14:42 -0800

    Revert "remove all controllers and jobs for testing, only 1 controller action left"
    This reverts commit c3faf778c426f1ae7c88d1da99632f5ad636192e.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-# resources :posts
+resources :posts
 # resources :posts expands to:
 # get "posts", to: "posts#index"
 # get "posts/new", to: "posts#new"
@@ -9,8 +9,7 @@
 # delete "posts/:id", to: "posts#delete"
 
 any "comments/hot", to: "comments#hot"
-# get "landing/posts", to: "posts#index"
+get "landing/posts", to: "posts#index"
 
-# get "admin/pages", to: "admin/pages#index"
-# get "related_posts/:id", to: "related_posts#show"
-
+get "admin/pages", to: "admin/pages#index"
+get "related_posts/:id", to: "related_posts#show"
