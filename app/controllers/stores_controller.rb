0325eb8b56fb392f421487782785b64b78d3c43b
Tung Nguyen <tongueroo@gmail.com>
2017-11-19 21:49:29 -0800

    add stores for testing

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,16 @@
 class StoresController < ApplicationController
+  # timeout 30
+  properties(dead_letter_config: "arn", timeout: 20, role: "myrole", memory_size: 1000)
+  def index
+  end
+
+  timeout 35
   def new
     @item = {name: "soda"}
+  end
+
+  environment(key1: "value1", key2: "value2")
+  memory_size(1024)
+  def show
   end
 end
