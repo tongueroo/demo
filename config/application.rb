491d3e43632ad45e9c81e3fe23e342b6009420f7
Tung Nguyen <tongueroo@gmail.com>
2017-11-20 17:43:27 -0800

    use env file for DB_URL

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 Jets.application.configure do
   config.project_name = "demo"
   # config.env_extra = 2
+  # config.extra_autoload_paths = []
 
   config.function.timeout = 10
   # config.function.memory_size = 1536
@@ -9,9 +10,13 @@ Jets.application.configure do
     global_app_key1: "global_app_value1",
     global_app_key2: "global_app_value2",
   }
-  config.function.dead_letter_queue = { target_arn: "arn" }
-  config.function.vpc_config = {
-    security_group_ids: [ "sg-1", "sg-2" ],
-    subnet_ids: [ "subnet-1", "subnet-2" ]
-  }
+  # More examples:
+  # config.function.dead_letter_queue = { target_arn: "arn" }
+  # config.function.vpc_config = {
+  #   security_group_ids: [ "sg-1", "sg-2" ],
+  #   subnet_ids: [ "subnet-1", "subnet-2" ]
+  # }
+  # The config.function settings to the CloudFormation Lambda Function properties.
+  # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-lambda-function.html
+  # Underscored format can be used for keys to make it look more ruby-ish.
 end
