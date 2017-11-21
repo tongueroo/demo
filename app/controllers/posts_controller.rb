a2e80528157956637533efa5d8e9d4d7470917c0
Tung Nguyen <tongueroo@gmail.com>
2017-11-20 16:43:12 -0800

    adding .env files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  class_timeout 22
+
+  timeout 18
   def index
     # puts "Post.table_name #{Post.table_name.inspect}"
     # posts = Post.scan # should not use scan for production
