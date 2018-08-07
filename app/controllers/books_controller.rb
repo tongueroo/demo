3c3d122ed1165ae73d2ba27cd72c96d3348857bc
Tung Nguyen <tongueroo@gmail.com>
2018-08-07 20:50:42 +0000

    poly node method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,7 @@
 class BooksController < ApplicationController
   python :show
   python :error_test
+
+  node :list
+  node :node_error_test
 end
---
+++ app/controllers/books_controller/node/list.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.handle = function(event, context, callback) {
+    var body = {'message': 'hi from node'};
+    var response = {
+      statusCode: "200",
+      headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(body)
+    };
+    callback(null, response);
+};
